b3521528df1b9c2241f6b94ff42fb6ab792fcc2e
LoralieFlint <loralie.flint0128@gmail.com>
2020-09-21 19:48:53 -0700

    return statement

--- algs.js
+++ algs.js
@@ -1,10 +1,27 @@
 // Print out each element of the following array ?// on a separate line:
-// Print out each element of the following array 
+// Print out each element of the following array
 // ['Joe', 2, 'Ted', 4.98, 14, 'Sam', 'void *', '42', 'float', 'pointers', 5006]
 // Verbalize your thought process as much as possible before writing any code. Run through the UPER problem solving framework while going through your thought process.
-const list = ['Joe', 2, 'Ted', 4.98, 14, 'Sam', 'void *', '42', 'float', 'pointers', 5006]
-for(let i = 0; i < list.length; i++) {
-  console.log(list[i])
+const list = [
+  "Joe",
+  2,
+  "Ted",
+  4.98,
+  14,
+  "Sam",
+  "void *",
+  "42",
+  "float",
+  "pointers",
+  5006,
+];
+for (let i = 0; i < list.length; i++) {
+  console.log(list[i]);
   // return list[i]
 }
 
+// Return Something to Me!
+// Write a function that returns the string "something" joined with a space " " and the given argument a.
+function giveMeSomething(a) {
+  return "something " + a;
+}
