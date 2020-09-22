3457e008e2160f56f6f482fc85411d905e4b07fe
LoralieFlint <loralie.flint0128@gmail.com>
2020-09-22 10:48:36 -0700

    3 edabit challanges

--- algs.js
+++ algs.js
@@ -25,3 +25,29 @@ for (let i = 0; i < list.length; i++) {
 function giveMeSomething(a) {
   return "something " + a;
 }
+
+// Profitable Gamble
+// Create a function that takes three arguments prob, prize, pay and returns true if prob * prize > pay; otherwise return false.
+function profitableGamble(prob, prize, pay) {
+	if (prob * prize > pay) {
+		return true
+	} else {
+		return false
+	}
+}
+
+// Return the First Element in an Array
+// Create a function that takes an array and returns the first element.
+function getFirstValue(arr) {
+	return arr[0]
+}
+
+// Football Points
+// Create a function that takes the number of wins, draws and losses and calculates the number of points a football team has obtained so far.
+// wins get 3 points
+// draws get 1 point
+// losses get 0 points
+function footballPoints(wins, draws, losses) {
+	return ((wins * 3) + (draws*1))
+}
+
